326716ebec46ee5d72bf77f7bac3e1a6b271036a
Blazk <jumilocaza@gmail.com>
2026-08-16 18:51:29 -0500

    Estructura Spring Boot

---
+++ src/main/java/com/poli/polirestaurante/PolirestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.poli.polirestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolirestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolirestauranteApplication.class, args);
+	}
+
+}
